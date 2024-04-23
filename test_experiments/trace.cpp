48b8b7766c3c57669471ad2e07f5011b0c4e9986
shlok <ashlok@umich.edu>
2024-04-23 10:13:32 -0400

    Trace build now works static ID added

--- test_experiments/trace.cpp
+++ test_experiments/trace.cpp
@@ -2,52 +2,45 @@
 #include <boost/dynamic_bitset.hpp>
 #include "ocelot/trace/interface/TraceGenerator.h"
 #include "ocelot/trace/interface/TraceEvent.h"
+#include <iostream>
 
 class TraceGenerator : public trace::TraceGenerator
 {
 public:
-	void event(const trace::TraceEvent &event)
-	{
-        // Here are the block dimensions
-        int bdimx = event.blockDim.x;
-        int bdimy = event.blockDim.y;
-        int bdimz = event.blockDim.z;
-        // Following is the format of the output
-        // blockidx blockidy blockidz threadidx threadyidx threadidz instruction memory_addresses
+    	void event(const trace::TraceEvent &event)
+    	{
+    	// Here are the block dimensions
+    	int bdimx = event.blockDim.x;
+    	int bdimy = event.blockDim.y;
+    	int bdimz = event.blockDim.z;
+    	// Following is the format of the output
+    	// blockidx blockidy blockidz threadidx threadyidx threadidz instruction memory_addresses
     	if (event.instruction->isMemoryInstruction()){
-            // so now get the block id of the event first
-            int blockidx = event.blockId.x;
-            int blockidy = event.blockId.y;
-            int blockidz = event.blockId.z;
-            // now get the thread id of the event
-            int threadidx = 0; // rows
-            int threadidy = 0; // column
-            int threadidz = 0;
+        	// so now get the block id of the event first
+        	int blockidx = event.blockId.x;
+        	int blockidy = event.blockId.y;
+        	int blockidz = event.blockId.z;
+        	// now get the thread id of the event
+        	int threadidx = 0; // rows
+        	int threadidy = 0; // column
+        	int threadidz = 0;
+        	auto vec = event.memory_addresses;
+        	int memory_counter = 0;
+        	for (size_t i = 0; i < event.active.size(); ++i) {
+            	if (event.active[i]) {
+                	// Now compute the x index based on the thread geometry
+                	threadidz = i / (bdimx * bdimy); // Computing z ID
 
-            for (size_t i = 0; i < event.active.size(); ++i) {
-                if (event.active[i]) {
-                    // Now compute the x index based on the thread geometry
-                    threadidz = i / (bdimx * bdimy); // Computing z ID
+                	threadidx = i % bdimx; // Computing x ID
+                	threadidy = (i / bdimx) % bdimy; // Computing y ID
 
-                    threadidx = i % bdimx; // Computing x ID
-                    threadidy = (i / bdimx) % bdimy; // Computing y ID
-                    
-                    std::cout << blockidx << " " << blockidy << " " << blockidz << " " << threadidx << " " << threadidy << " " << threadidz << " " << event.instruction->toString() << " ";
-                    // Printing active threads information (Assuming that there will be as many memory addresses as active threads)
-                    std::cout << event.memory_addresses[i] << std::endl;
+                	std::cout << blockidx << " " << blockidy << " " << blockidz << " " << threadidx << " " << threadidy << " " << threadidz << " " << event.instruction->toString() << " ";
+                	// Printing active threads information (Assuming that there will be as many memory addresses as active threads)
+                    	std::cout << vec[memory_counter] << '\n';
+                    	memory_counter++;
 
-                }
-            }
-        }
-	}
+            	}
+        	}
+    	}
+    }
 };
-
-extern void sampleKernel();
-
-int main()
-{
-    	TraceGenerator generator;
-    	ocelot::addTraceGenerator( generator );
-
-    	sampleKernel();
-}
